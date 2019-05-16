b69033ce128bd2e9bb49c342f7db7be4416cf299
Alex Colombo <uricaine@hotmail.com>
2019-05-16 13:27:43 +0200

    shared module created

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  exports: [CommonModule, ReactiveFormsModule, IonicModule]
+})
+export class SharedModule {}

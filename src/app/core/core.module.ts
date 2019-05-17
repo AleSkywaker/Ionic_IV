b70c8915a4bc2252194b2f188e13f6099277b457
Alex Colombo <uricaine@hotmail.com>
2019-05-17 18:36:00 +0200

    add angularFireModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,8 +5,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
+import { AngularFireModule } from '@angular/fire';
 @NgModule({
-  imports: [IonicModule.forRoot()],
+  imports: [IonicModule.forRoot(), AngularFireModule],
   exports: [BrowserModule, IonicModule],
   providers: [
     StatusBar,

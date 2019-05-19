291ca22cd37418f181085a1c61a9904289745377
Alex Colombo <uricaine@hotmail.com>
2019-05-19 10:56:54 +0200

    sigInWithPopUp method added

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase';
+import { User, AuthProvider } from './auth.types';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,28 @@ import { auth } from 'firebase';
 export class AuthService {
   constructor(private afAuth: AngularFireAuth) {}
 
-  private sigInWithEmail({ email, password }): Promise<auth.UserCredential> {
+  private signInWithEmail({ email, password }: User): Promise<auth.UserCredential> {
     return this.afAuth.auth.signInWithEmailAndPassword(email, password);
   }
-  private sigUpWithEmail({ email, password, name }): Promise<auth.UserCredential> {
+  private signUpWithEmail({ email, password, name }: User): Promise<auth.UserCredential> {
     return this.afAuth.auth.createUserWithEmailAndPassword(email, password).then(credentials => {
       return credentials.user
         .updateProfile({ displayName: name, photoURL: null })
         .then(() => credentials);
     });
+  }
+
+  private signInWithPopUp(provider: AuthProvider): Promise<auth.UserCredential> {
+    let signInProvider = null;
+
+    switch (provider) {
+      case AuthProvider.Facebook:
+        signInProvider = new auth.FacebookAuthProvider();
+        break;
+      case AuthProvider.Google:
+        signInProvider = new auth.GoogleAuthProvider();
+        break;
+    }
+    return this.afAuth.auth.signInWithPopup(signInProvider);
   }
 }

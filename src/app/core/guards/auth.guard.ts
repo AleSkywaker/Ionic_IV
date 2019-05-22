eb8ef25863d96f308fc8277ed88032f8f81ce0ab
Alex Colombo <uricaine@hotmail.com>
2019-05-22 21:25:21 +0200

    creating guards

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard {}
+export class AuthGuard {
+  constructor(private authService: AuthService) {}
+
+  private checkAuthState(redirect: string): Observable<boolean> {}
+}

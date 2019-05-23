cbf3cdffe9011844b5c7bbe4c30f08c68f46695f
Alex Colombo <uricaine@hotmail.com>
2019-05-23 20:36:28 +0200

    creating guards

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -10,6 +10,15 @@ export class AuthGuard {
   constructor(private authService: AuthService) {}
 
   private checkAuthState(redirect: string): Observable<boolean> {
-    return this.authService.isAtuthenticate;
+    return this.authService.isAtuthenticate.pipe(
+      tap(is => {
+        if (!is) {
+          this.router.navigate(['/login'], {
+            // /login?redirect=/tasks/create
+            queryParams: { redirect }
+          });
+        }
+      })
+    );
   }
 }

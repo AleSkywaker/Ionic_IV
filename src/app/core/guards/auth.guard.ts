33fd16721078174eda9dc7862afc3e1fcc5ed419
Alex Colombo <uricaine@hotmail.com>
2019-05-23 20:45:00 +0200

    implementing canActivate

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -16,6 +16,9 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.checkAuthState(state.url);
+  }
   private checkAuthState(redirect: string): Observable<boolean> {
     return this.authService.isAtuthenticate.pipe(
       tap(is => {

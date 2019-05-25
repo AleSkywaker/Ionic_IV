aa2b86383d59a9643370b95986e9ff33dbaadecd
Alex Colombo <uricaine@hotmail.com>
2019-05-25 09:40:00 +0200

    delete comom module in task module

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { TasksRoutingModule } from './tasks-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    TasksRoutingModule
-  ]
+  imports: [TasksRoutingModule]
 })
-export class TasksModule { }
+export class TasksModule {}

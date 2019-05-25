d60f10f29ee23f3098855127ba9467610b52e80c
Alex Colombo <uricaine@hotmail.com>
2019-05-25 10:53:36 +0200

    setting tasks routing module

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
+    canActivateChild: [AuthGuard],
     children: [
       {
         path: '',
-        loadChildren: './pages/tasks-list/tasks-list.module#TasksListPageModule'
+        loadChildren: './pages/tasks-list/tasks-list.module#TasksListPageModule',
+        canLoad: [AuthGuard]
       }
     ]
   }

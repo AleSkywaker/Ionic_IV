66860d7f1095aa63cfe9c5ea1fa7f9c67b57b759
Alex Colombo <uricaine@hotmail.com>
2019-05-26 19:07:14 +0200

    new task interface

--- src/app/tasks/components/task-item/task-item.component.ts
+++ src/app/tasks/components/task-item/task-item.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.scss'],
+  styleUrls: ['./task-item.component.scss']
 })
-export class TaskItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
-}
+export class TaskItemComponent {}

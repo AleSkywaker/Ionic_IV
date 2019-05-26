6a1087ed0a7edc7ed97dc10d62945cafd711ace5
Alex Colombo <uricaine@hotmail.com>
2019-05-26 19:10:39 +0200

    creating task item component

--- src/app/tasks/components/task-item/task-item.component.ts
+++ src/app/tasks/components/task-item/task-item.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.scss']
 })
-export class TaskItemComponent {}
+export class TaskItemComponent {
+  // <app-task-item [task]="task"></app-task-item>
+  @Input() task: Task;
+  @Output() done = new EventEmitter<Task>();
+}

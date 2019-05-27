463bf997dfb0f21621c0d0a6723738b2a0665680
Alex Colombo <uricaine@hotmail.com>
2019-05-27 19:00:50 +0200

    working in task list cmodule

--- src/app/tasks/pages/tasks-list/tasks-list.module.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
-import { IonicModule } from '@ionic/angular';
-
 import { TasksListPage } from './tasks-list.page';
+import { ComponentsModule } from '../../components/components.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
   {
@@ -15,12 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
+  imports: [ComponentsModule, SharedModule, RouterModule.forChild(routes)],
   declarations: [TasksListPage]
 })
 export class TasksListPageModule {}

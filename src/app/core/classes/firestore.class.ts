9cd42f5fced84031cf7f8de20c05190fc05cd68f
Alex Colombo <uricaine@hotmail.com>
2019-05-28 21:56:19 +0200

    working firestore abstract class

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -1,13 +1,19 @@
 import { AngularFirestore, AngularFirestoreCollection, QueryFn } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-export abstract class Firestore<T> {
+export abstract class Firestore<T extends { id: string }> {
   protected collection: AngularFirestoreCollection<T>;
 
   constructor(protected db: AngularFirestore) {}
 
   protected setCollection(path: string, queryFn?: QueryFn): void {
     this.collection = path ? this.db.collection(path, queryFn) : null;
+  }
+
+  private setItem(item: T, operation: string): Promise<T> {
+    this.collection // /users/someID/tasks/anotherID
+      .doc<T>(item.id);
+    return Promise.resolve(null);
   }
 
   public getAll(): Observable<T[]> {

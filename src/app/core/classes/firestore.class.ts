234e602b1561ae59e57bb58dd68dbf8171e7bad8
Alex Colombo <uricaine@hotmail.com>
2019-05-28 22:47:30 +0200

    added delete method to firestore.class

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -9,14 +9,12 @@ export abstract class Firestore<T extends { id: string }> {
   protected setCollection(path: string, queryFn?: QueryFn): void {
     this.collection = path ? this.db.collection(path, queryFn) : null;
   }
-
   private setItem(item: T, operation: string): Promise<T> {
     return this.collection // /users/someID/tasks/anotherID
       .doc<T>(item.id)
       [operation](item)
       .then(() => item);
   }
-
   public getAll(): Observable<T[]> {
     return this.collection.valueChanges();
   }

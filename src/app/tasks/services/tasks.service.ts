c8eaa8764b09598f159880b08dc361c4d5edde6b
Alex Colombo <uricaine@hotmail.com>
2019-05-29 21:23:55 +0200

    creating init method

--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Firestore } from 'src/app/core/classes/firestore.class';
 import { Task } from '../models/task.model';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService extends Firestore<Task> {
-  constructor(db: AngularFirestore) {
+  constructor(private authService: AuthService, db: AngularFirestore) {
     super(db);
+    this.init();
   }
+
+  private init(): void {}
 }

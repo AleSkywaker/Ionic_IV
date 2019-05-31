c8fdeeefcd3b0123adfee72daa100a9cc361e5d2
Alex Colombo <uricaine@hotmail.com>
2019-05-31 20:58:17 +0200

    added task-save-module

---
+++ src/app/tasks/pages/task-save/task-save.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TaskSavePage } from './task-save.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TaskSavePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TaskSavePage]
+})
+export class TaskSavePageModule {}

1b4ea41866bea368c6ea79a7d91a72768be50737
Alex Colombo <uricaine@hotmail.com>
2019-06-06 21:44:45 +0200

    added delete method

--- src/app/tasks/pages/tasks-list/tasks-list.page.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.page.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { Task } from '../../models/task.model';
 import { TasksService } from '../../services/tasks.service';
 import { NavController } from '@ionic/angular';
+import { OverlayService } from 'src/app/core/services/overlay.service';
 
 @Component({
   selector: 'app-tasks-list',
@@ -11,7 +12,11 @@ import { NavController } from '@ionic/angular';
 })
 export class TasksListPage implements OnInit {
   tasks$: Observable<Task[]>;
-  constructor(private navCtrl: NavController, private taskService: TasksService) {}
+  constructor(
+    private navCtrl: NavController,
+    private overlayService: OverlayService,
+    private taskService: TasksService
+  ) {}
 
   ngOnInit() {
     this.tasks$ = this.taskService.getAll();
@@ -21,5 +26,22 @@ export class TasksListPage implements OnInit {
     // console.log('evento ', task.title);
     this.navCtrl.navigateForward(`/tasks/edit/${task.id}`);
     // this.navCtrl.navigateForward(['tasks', 'edit', task.id]);
+  }
+  async onDelete(task: Task): Promise<void> {
+    await this.overlayService.alert({
+      message: `¿Desea eliminar la tarea? ${task.title}?`,
+      buttons: [
+        {
+          text: 'Si',
+          handler: async () => {
+            await this.taskService.delete(task);
+            await this.overlayService.toast({
+              message: `Tarea ${task.title} eliminada!`
+            });
+          }
+        },
+        'No'
+      ]
+    });
   }
 }

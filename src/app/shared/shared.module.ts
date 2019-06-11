b1732cd248f6a833f029ba535a8333bbabe256e8
Alex Colombo <uricaine@hotmail.com>
2019-06-11 21:22:27 +0200

    abstract button from task list template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
 
 @NgModule({
-  exports: [CommonModule, ReactiveFormsModule, IonicModule]
+  declarations: [MenuToggleComponent],
+  imports: [IonicModule],
+  exports: [CommonModule, ReactiveFormsModule, IonicModule, MenuToggleComponent]
 })
 export class SharedModule {}

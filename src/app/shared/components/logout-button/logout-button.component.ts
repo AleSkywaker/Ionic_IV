3a30fbf27ccbc32054690a4e6ece986fe5b47dfe
Alex Colombo <uricaine@hotmail.com>
2019-06-15 19:45:24 +0200

    init logout template

--- src/app/shared/components/logout-button/logout-button.component.ts
+++ src/app/shared/components/logout-button/logout-button.component.ts
@@ -2,13 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logout-button',
-  templateUrl: './logout-button.component.html',
-  styleUrls: ['./logout-button.component.scss'],
+  template: ``
 })
 export class LogoutButtonComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }

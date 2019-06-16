d44232ae38362b6b3a1e6a8e6646902adb0e0bc4
Alex Colombo <uricaine@hotmail.com>
2019-06-16 13:56:04 +0200

    inyect services to logout buttons component

--- src/app/shared/components/logout-button/logout-button.component.ts
+++ src/app/shared/components/logout-button/logout-button.component.ts
@@ -1,17 +1,40 @@
+import { OverlayService } from './../../../core/services/overlay.service';
+import { NavController } from '@ionic/angular';
+import { AuthService } from './../../../core/services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logout-button',
   template: `
     <ion-buttons>
-      <ion-button>
+      <ion-button (click)="logout()">
         <ion-icon name="exit" slot="icon-only"></ion-icon>
       </ion-button>
     </ion-buttons>
   `
 })
 export class LogoutButtonComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private navCtrl: NavController,
+    private overlayService: OverlayService
+  ) {}
 
   ngOnInit() {}
+
+  async logout(): Promise<void> {
+    await this.overlayService.alert({
+      message: '¿Desea salir de la aplicación?',
+      buttons: [
+        {
+          text: 'Si',
+          handler: async () => {
+            await this.authService.logout();
+            this.navCtrl.navigateRoot('/login');
+          }
+        },
+        'No'
+      ]
+    });
+  }
 }

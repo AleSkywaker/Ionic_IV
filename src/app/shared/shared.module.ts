34461dd91fe1f3b635623e3614ece772501f12ef
Alex Colombo <uricaine@hotmail.com>
2019-06-16 13:55:38 +0200

    added LogOut button component to shared modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
 import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
 
 @NgModule({
-  declarations: [MenuToggleComponent],
+  declarations: [LogoutButtonComponent, MenuToggleComponent],
   imports: [IonicModule],
-  exports: [CommonModule, ReactiveFormsModule, IonicModule, MenuToggleComponent]
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule,
+    LogoutButtonComponent,
+    MenuToggleComponent
+  ]
 })
 export class SharedModule {}
